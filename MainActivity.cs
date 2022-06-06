74a057c3a7acb5d4e8dc84972e222faa671c1fc8
Jakub Filipiak (filipiakj) <filipiakj@coventry.ac.uk>
2022-06-06 20:54:17 +0200

    Removed useless comments.

--- MainActivity.cs
+++ MainActivity.cs
@@ -26,30 +26,6 @@ namespace functional_bubble.NET
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
-            /*
-            mainListView = FindViewById<ListView>(Resource.Id.MainView);
-            mItems = new List<Task>();
-
-            ListViewAdapter adapter = new ListViewAdapter(this, mItems);
-            mainListView.Adapter = adapter;
-
-            mBtnNewTask = FindViewById<Button>(Resource.Id.activity_main_buttonNewTask);
-            mBtnNewTask.Click += (object sender, EventArgs e) =>
-            {
-                //Method for creating DialogFragment from Dialog_NewTask Class
-                Dialog_NewTask newTaskDialog = new Dialog_NewTask(); //Create a new Dialog Fragment
-                newTaskDialog.Show(SupportFragmentManager,"Dialog"); //Show on screen the Dialog Fragment
-
-                newTaskDialog.mNewTaskComplete += (object sender, onNewTaskEventArgs e) =>
-                {
-                    //Method executed when onNewTaskEventArgs in Dialog_newTask is Invoked
-                    adapter.Add(e.mNewTaskInEvent); //Add Task from onNewTaskEventArgs class as a new list row in Task UI
-                    adapter.NotifyDataSetChanged(); //Refresh Task UI
-
-                };
-            };
-            */
-
             //Fragment manager is used to dynamically replace fragments that are currently part of the activity.
             fragmentManager = SupportFragmentManager.BeginTransaction();        //Calling this method returns an instance of FragmentTransaction
             fragmentManager.Add(Resource.Id.replacableContainer, new TodoBase());  //Create a new instance of TodoBase (our default screen when app is opened) and
