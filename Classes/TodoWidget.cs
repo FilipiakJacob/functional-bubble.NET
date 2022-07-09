2560e1901586c1320a333113482be242e1af90ee
Jakub Filipiak <filipiakjacob@gmail.com>
2022-07-09 20:52:04 +0200

    Widget now properly updates itself.

--- Classes/TodoWidget.cs
+++ Classes/TodoWidget.cs
@@ -20,16 +20,22 @@ namespace functional_bubble.NET.Classes
     {
 
         public override void OnUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+            //Method called when widget is updated
         {
+            //Example: create widget, resize, meet update time, manually updated
             var me = new ComponentName(context, Java.Lang.Class.FromType(typeof(TodoWidget)).Name);
             appWidgetManager.UpdateAppWidget(me, BuildRemoteViews(context, appWidgetIds));
         }
         
         private RemoteViews BuildRemoteViews(Context context, int[] appWidgetIds)
         {
+            //Build view
             var widgetView = new RemoteViews(context.PackageName, Resource.Layout.todo_widget);
 
+            //Change text in widget
             SetTextViewText(widgetView);
+
+            //Handle buttons in widget
             RegisterClicks(context, appWidgetIds, widgetView);
 
             return widgetView;
@@ -46,14 +52,18 @@ namespace functional_bubble.NET.Classes
             intent.SetAction(AppWidgetManager.ActionAppwidgetUpdate);
             intent.PutExtra(AppWidgetManager.ExtraAppwidgetIds, appWidgetIds);
 
-            // Register click event for the Background
-            var piBackground = PendingIntent.GetBroadcast(context, 0, intent, PendingIntentFlags.UpdateCurrent);
-            widgetView.SetOnClickPendingIntent(Resource.Id.todo_widget_text, piBackground);
+            // Register click event for button1
+            var widgetButton1 = PendingIntent.GetBroadcast(context, 0, intent, PendingIntentFlags.UpdateCurrent);
+            widgetView.SetOnClickPendingIntent(Resource.Id.button1, widgetButton1);
+
+            
         }
 
 
         public override void OnReceive(Context context, Intent intent)
+            //Method is called when the widget receives an intent broadcast.
         {
+            base.OnReceive(context, intent); //Since OnReceive is overrridden, need to call base class to make sure OnUpdate and similar methods ale triggered by this method.
             Toast.MakeText(context, "Received intent!", ToastLength.Short).Show();
         }
     }
