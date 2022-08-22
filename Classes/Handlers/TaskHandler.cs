356a99db34e0171588a5ee6a9c5336bc1262769b
petriNicholas <93609490+petriNicholas@users.noreply.github.com>
2022-08-22 19:54:56 +0200

    Feature/pen 165 sort methods handlers (#33)
    * - added GetSortedTasks() method to TaskHandler.cs
    
    * - added const numberOfPriorities variable
    
    - added SortedByDeadlineTasks method to TaskHandler.cs
    
    * - const var is now uppercase
    
    * - now GetSortedTasks also sort by deadline
    - consts are now fixed
    
    * - fixed condition in for loop

--- Classes/Handlers/TaskHandler.cs
+++ Classes/Handlers/TaskHandler.cs
@@ -8,11 +8,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using static Android.Provider.UserDictionary;
 
 namespace functional_bubble.NET.Classes
 {
     public class TaskHandler : DatabaseHandler
     {
+        public const int LOWEST_PRIORITY = 0;
+        public const int HIGHEST_PRIORITY = 3;
+
         public void Add(Task task) // inserts task object to Task table
         {
             _db.Insert(task);    
@@ -45,6 +49,27 @@ namespace functional_bubble.NET.Classes
         {
             List<Task> allTasks = _db.Query<Task>("SELECT * FROM Tasks");
             return allTasks;
+        }
+
+        public List<Task> GetSortedTasks() // return sorted tasks by priorities and deadline
+        {
+            List<Task> sortedTasks = new List<Task>();
+            List<Task> tempTasks = new List<Task>();
+
+            for (int i = HIGHEST_PRIORITY; i >= LOWEST_PRIORITY; i--)
+            {
+                tempTasks = _db.Query<Task>("SELECT * FROM Tasks WHERE Priority={0}", i);
+                SortedByDeadlineTasks(tempTasks);
+                sortedTasks.AddRange(tempTasks);
+            }
+
+            return sortedTasks;
+        }
+
+        public List<Task> SortedByDeadlineTasks(List<Task> tasks) // sort list of tasks by deadline
+        {
+            tasks.Sort((a, b) => a.Deadline.CompareTo(b.Deadline));
+            return tasks;
         }
 
         //@author Mateusz Staszek
