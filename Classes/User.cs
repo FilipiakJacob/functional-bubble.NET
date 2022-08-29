f67a58ff7cdf35845ab2ce71febd37308a1f9ffd
Nicholas Petri <petrimikolaj@gmail.com>
2022-08-29 16:39:46 +0200

    - added User class

---
+++ Classes/User.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLite;
+
+namespace functional_bubble.NET.Classes
+{
+    [Table("User")]
+    public class User
+    {
+        [PrimaryKey, Unique, Column("id")]
+        public int Id { get; set; }
+        [Column("Username")]
+        public string Username { get; set; }
+        [Column("LastCompletedTaskDate")]
+        public DateTime LastCompletedTaskDate { get; set; }
+        [Column("Streak")]
+        public bool StreakIsActive { get; set; }
+        [Column("Money")]
+        public int Money { get; set; }
+    }
+}
