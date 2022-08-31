24a5c2dbc54d4860fde1f9217e44b9a3944d5051
Nicholas Petri <petrimikolaj@gmail.com>
2022-08-31 14:24:19 +0200

    - added methods to increase readabilty of the code - added comments

--- Classes/Handlers/UserHandler.cs
+++ Classes/Handlers/UserHandler.cs
@@ -4,6 +4,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using AndroidX.Emoji2.Text.FlatBuffer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,36 +14,32 @@ namespace functional_bubble.NET.Classes
 {
     public class UserHandler : DatabaseHandler
     {
-        public UserHandler() // constructor
+        public UserHandler() // constructor 
         {
             CheckTableIfOK();
         }
         public void CheckTableIfOK() // this method checks if table is up with assummed conditions 
         {
             List<User> table = this.GetAll();
-            if (table == null)
+            if (table == null) // if table is empty this method adds new user
             {
-                User user = new User();
-                _db.Insert(user);
+                InitialUserTable();
                 return;
             }
-            if(table.Count > 1)
+            if(table.Count > 1) // if table has more then one user it deletes additional users
             {
-                for (int i = 1; i < table.Count; i++) {
-                    _db.Query<User>("DELETE FROM User WHERE id={0}", i);
-                }
+                DeleteAdditionalUsers(table);
                 return;
             }
             return;
         }
-
-        public List<User> GetAll()
+        public List<User> GetAll() //method used for checking if user table is ok
         {
             List<User> userTable = _db.Query<User>("SELECT * FROM User");
             return userTable;
         }
 
-        public User GetUser()
+        public User GetUser() // method used for all operations on user
         {
             User user = _db.Get<User>(0);
             return user;
@@ -60,6 +57,7 @@ namespace functional_bubble.NET.Classes
                 user.StreakIsActive = true;
                 user.StreakCount++;
 
+                Update(user);
                 return;
             }
             //condition for situation when user completes multiple tasks in one day
@@ -74,7 +72,27 @@ namespace functional_bubble.NET.Classes
             {
                 user.StreakCount = 0;
                 user.StreakIsActive= false;
+                Update(user);
                 return;
+            }
+        }
+
+        public void Update(User user) //updates user in database
+        {
+            _db.Update(user);
+        }
+
+        public void InitialUserTable() //Initial state of the user table
+        {
+            User user = new User();
+            _db.Insert(user);
+        }
+
+        public void DeleteAdditionalUsers(List<User> userTable) //deletes all additional users in table
+        {
+            for (int i = 1; i < userTable.Count; i++)
+            {
+                _db.Query<User>("DELETE FROM User WHERE id={0}", i);
             }
         }
     }
