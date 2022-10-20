fe8fddddc7e1971fff3719776c4004fd03e6067a
Nicholas Petri <petrimikolaj@gmail.com>
2022-10-20 12:47:18 +0200

    - user.cs refactoring

--- Classes/User.cs
+++ Classes/User.cs
@@ -24,21 +24,45 @@ namespace functional_bubble.NET.Classes
     {
         [PrimaryKey, Unique, Column("id")]
         public int Id { get; set; } = 0;
+        /// <summary>
+        /// var with not any particular usage rn but maybe in the future will be used to idk personalised encouragement messages?
+        /// </summary>
         [Column("Username")]
-        public string Username { get; set; } // var with not any particular usage rn but maybe in the future will be used to idk personalised encouragement messages?
+        public string Username { get; set; }
+        /// <summary>
+        /// variable that is used for checking streak
+        /// </summary>
         [Column("LastCompletedTaskDate")]
-        public DateTime LastCompletedTaskDate { get; set; }// variable that is used for checking streak
+        public DateTime LastCompletedTaskDate { get; set; }
+        /// <summary>
+        /// variable that tells if user is on hot streak or not
+        /// </summary>
         [Column("Streak")]
-        public bool StreakIsActive { get; set; } = false; // variable that tells if user is on hot streak or not
+        public bool StreakIsActive { get; set; } = false;
+        /// <summary>
+        /// variable that tells how many days in the row user did a task
+        /// </summary>
         [Column("StreakCount")]
-        public int StreakCount { get; set; } = 0;// variable that tells how many days in the row user did a task
+        public int StreakCount { get; set; } = 0;
+        /// <summary>
+        /// variable that shows how much money user has
+        /// </summary>
         [Column("Money")]
-        public int Money { get; set; } = 0;// variable that shows how much money user has
+        public int Money { get; set; } = 0;
+        /// <summary>
+        /// var that shows all created tasks ever number
+        /// </summary>
         [Column("TotalTasks")]
-        public int TotalTasks { get; set; } = 0; //var that shows all created tasks ever number
+        public int TotalTasks { get; set; } = 0;
+        /// <summary>
+        /// var that shows all completed tasks number
+        /// </summary>
         [Column("CompletedTasks")]
-        public int CompletedTasks { get; set;} = 0; //var that shows all completed tasks number
+        public int CompletedTasks { get; set;} = 0;
+        /// <summary>
+        /// var that shows all abandoned tasks number
+        /// </summary>
         [Column("AbandonedTasks")]
-        public int AbandonedTasks { get; set ; } = 0; //var that shows all abandoned tasks number
+        public int AbandonedTasks { get; set ; } = 0;
     }
 }
