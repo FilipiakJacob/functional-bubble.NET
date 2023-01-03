db4501ef1da05d31e14c3a2aabaa53d638a7ba65
Mikołaj Petri <petrimikolaj@gmail.com>
2023-01-03 18:11:02 +0100

    - added Notification method, for now it only pushes notification to the user screaming TEST TEST TEST TEST

--- Classes/Handlers/NotificationHandler.cs
+++ Classes/Handlers/NotificationHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Content.Res;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -8,14 +9,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using static Android.Util.EventLogTags;
 
 namespace functional_bubble.NET.Classes.Handlers
 {
     public class NotificationHandler
     {
-        private static readonly int NOTIFICATION_ID = 1000;
+
+        private static readonly int NOTIFICATION_ID = 1000; 
         private static readonly string CHANNEL_ID = "local_notification";
 
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Notification()
+        {
+            var builder = new Notification.Builder(Application.Context, CHANNEL_ID).SetAutoCancel(true)
+                .SetContentTitle("Task Created")
+                .SetSmallIcon(Resource.Drawable.ic_clock_black_24dp)
+                .SetContentText($"TEST TEST TEST TEST");
+
+            var nmc = NotificationManager.FromContext(Application.Context);
+            nmc.Notify(NOTIFICATION_ID, builder.Build());
+        }
 
     }
 }
