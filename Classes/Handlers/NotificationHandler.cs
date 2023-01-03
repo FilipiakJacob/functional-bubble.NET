cd034e2ccfe1bcbfedfd686df5888c13647c54b1
Mikołaj Petri <petrimikolaj@gmail.com>
2023-01-03 16:59:58 +0100

    - created NotificationHandler.cs and added private fields

---
+++ Classes/Handlers/NotificationHandler.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace functional_bubble.NET.Classes.Handlers
+{
+    public class NotificationHandler
+    {
+        private static readonly int NOTIFICATION_ID = 1000;
+        private static readonly string CHANNEL_ID = "local_notification";
+
+
+    }
+}
