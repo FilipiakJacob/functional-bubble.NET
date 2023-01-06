4a32db4750d6ddd085fe5fa06cd89399ef1ca56a
Mikołaj Petri <petrimikolaj@gmail.com>
2023-01-06 14:12:06 +0100

    - deeplinkHandler implemented into NotificationHandler.cs

--- Classes/Handlers/NotificationHandler.cs
+++ Classes/Handlers/NotificationHandler.cs
@@ -18,16 +18,30 @@ namespace functional_bubble.NET.Classes.Handlers
 
         private static readonly int NOTIFICATION_ID = 1000; 
         private static readonly string CHANNEL_ID = "local_notification";
+        public int taskID;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="taskID">int taskID - id of the task that is destination of notification</param>
+        public NotificationHandler(int taskID)
+        {
+            this.taskID = taskID;
+        }
 
         /// <summary>
         /// sends notification to the user screaming TEST TEST TEST TEST
         /// </summary>
         public void Notification()
         {
+            DeeplinkHandler deeplinkHandler = new DeeplinkHandler();
+
             var builder = new Notification.Builder(Application.Context, CHANNEL_ID).SetAutoCancel(true)
                 .SetContentTitle("Task Created")
+                .SetContentIntent
+                (deeplinkHandler.CreateDeeplink(Application.Context, Resource.Id.dest_task, "taskID", taskID))
                 .SetSmallIcon(Resource.Drawable.ic_clock_black_24dp)
-                .SetContentText($"TEST TEST TEST TEST");
+                .SetContentText($"DEEPLINK");
 
             var nmc = NotificationManager.FromContext(Application.Context);
             nmc.Notify(NOTIFICATION_ID, builder.Build());
